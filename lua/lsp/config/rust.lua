03eec4177e62cd93089cc14c1521bb677b4b71e7
secheng722 <826216325@qq.com>
2022-11-23 00:55:05 +0800

    Update rust.lua

--- lua/lsp/config/rust.lua
+++ lua/lsp/config/rust.lua
@@ -1,34 +1,48 @@
 local opts = {
   capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
-  flags = {
-    debounce_text_changes = 150,
+  tools = {
+    runnables = {
+      use_telescope = true,
+    },
+    inlay_hints = {
+      auto = true,
+      show_parameter_hints = false,
+      parameter_hints_prefix = "",
+      other_hints_prefix = "",
+    },
   },
-  on_attach = function(client, bufnr)
-    -- 禁用格式化功能，交给专门插件插件处理
-    client.resolved_capabilities.document_formatting = false
-    client.resolved_capabilities.document_range_formatting = false
-    local function buf_set_keymap(...)
-      vim.api.nvim_buf_set_keymap(bufnr, ...)
-    end
-    -- 绑定快捷键
-    require("keybindings").mapLSP(buf_set_keymap)
-  end,
+
+  -- all the opts to send to nvim-lspconfig
+  -- these override the defaults set by rust-tools.nvim
+  -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
+  server = {
+    -- on_attach is a callback called when the language server attachs to the buffer
+    on_attach = function(client, bufnr)
+      -- 禁用格式化功能，交给专门插件插件处理
+      client.resolved_capabilities.document_formatting = false
+      client.resolved_capabilities.document_range_formatting = false
+      local function buf_set_keymap(...)
+        vim.api.nvim_buf_set_keymap(bufnr, ...)
+      end
+
+      -- 绑定快捷键
+      require("keybindings").mapLSP(buf_set_keymap)
+    end,
+  },
+  settings = {
+    -- to enable rust-analyzer settings visit:
+    -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+    ["rust-analyzer"] = {
+      -- enable clippy on save
+      checkOnSave = {
+        command = "clippy",
+      },
+    },
+  },
 }
 
 return {
-  on_setup = function(server)
-    -- Initialize the LSP via rust-tools instead
-    require("rust-tools").setup({
-      -- The "server" property provided in rust-tools setup function are the
-      -- settings rust-tools will provide to lspconfig during init.
-      -- We merge the necessary settings from nvim-lsp-installer (server:get_default_options())
-      -- with the user's own settings (opts).
-      server = vim.tbl_deep_extend("force", server:get_default_options(), opts),
-    })
-    server:attach_buffers()
-    -- Only if standalone support is needed
-    require("rust-tools").start_standalone_if_required()
-  end,
+  on_setup = function()
+    require("rust-tools").setup(opts)
+  end
 }
-
-
