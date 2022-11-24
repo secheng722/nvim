420c88c74435f268acff1554b0aa539c80ff0cc4
secheng722 <826216325@qq.com>
2022-11-24 18:05:13 +0800

    Update rust.lua

--- lua/dap/nvim-dap/config/rust.lua
+++ lua/dap/nvim-dap/config/rust.lua
@@ -1,6 +1,6 @@
 -- 这里要修改为你的绝对路径
 local extension_path =
-  "/Users/lishaocheng/.local/share/nvim/site/pack/packer/start/vimspector/gadgets/macos/download/CodeLLDB/v1.6.10/root/extension/"
+  "/Users/lishaocheng/.local/share/nvim/site/pack/packer/opt/vimspector/gadgets/macos/download/CodeLLDB/v1.7.4/root/extension/"
 local codelldb_path = extension_path .. "adapter/codelldb"
 local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
 
